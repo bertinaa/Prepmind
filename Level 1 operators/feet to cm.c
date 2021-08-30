203ad43b468ea3c83244a9d70a7c73a03cfc5a07
Bertina S <76837815+bertinaa@users.noreply.github.com>
2021-08-30 10:06:21 +0530

    Create feet to cm.c

---
+++ Level 1 operators/feet to cm.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+int main()
+{
+    double feet,cm;
+    scanf("%lf",&feet);
+    cm=feet*30.48;
+    printf("%.3lf",cm);
+
+    return 0;
+}
+
+
