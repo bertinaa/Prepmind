2c0a8720177235aaac82eba50f27089c0f32361b
Bertina S <76837815+bertinaa@users.noreply.github.com>
2021-08-30 10:07:19 +0530

    Create m to cm.c

---
+++ Level 1 operators/m to cm.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+int main()
+{
+    double num,ans;
+    scanf("%lf",&num);
+    ans=num*100;
+    printf("%.lf",ans);
+
+    return 0;
+}
