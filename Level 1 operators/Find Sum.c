b0f048325d8b2e72746daf0319b9af2b9b52f46d
Bertina S <76837815+bertinaa@users.noreply.github.com>
2021-09-01 00:01:37 +0530

    Create Find Sum.c

---
+++ Level 1 operators/Find Sum.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <math.h>
+int main()
+{
+    int n,sum;
+    scanf("%d",&n);
+    sum=((n*(n+1))/2);
+    printf("Sum of first %d numbers is %d",n,sum);
+
+
+    return 0;
+}
