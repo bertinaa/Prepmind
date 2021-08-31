bb19f3c735ffba305ea35a5bd8b7b3aa3d61b943
Bertina S <76837815+bertinaa@users.noreply.github.com>
2021-09-01 00:04:25 +0530

    Create Print The CGPA.c

---
+++ Input output statements/Print The CGPA.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main(){
+    
+    //program to get the cgpa and print it
+
+    double cgpa;
+    scanf("%lf",&cgpa);
+    printf("%.1lf",cgpa);
+
+
+    return 0;
+}
