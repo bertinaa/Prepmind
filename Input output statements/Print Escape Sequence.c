efb876743b97cc698d5e9ff21f316f5db495b027
Bertina S <76837815+bertinaa@users.noreply.github.com>
2021-09-03 01:54:37 +0530

    Create Print Escape Sequence.c

---
+++ Input output statements/Print Escape Sequence.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+    printf("\\a - alarm\n");
+    printf("\\b - backspace\n");
+    printf("\\r - carriage return\n");
+    printf("\\n - new line\n");
+    printf("\\t - horizontal tab\n");
+    printf("\\v - vertical tab\n");
+    printf("\\\\ - backslash\n");
+    printf("\' - single quote\n");
+    printf("\" - double quote");
+
+    return 0;
+}
