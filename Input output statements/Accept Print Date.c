91e3b0358e9c4936fbe01e8c5da03899501ad531
Bertina S <76837815+bertinaa@users.noreply.github.com>
2021-09-03 01:52:09 +0530

    Create Accept Print Date.c

---
+++ Input output statements/Accept Print Date.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+void main()
+{
+
+    int date,month,year;
+    scanf("%02d/%02d/%04d" , &date,&month,&year);
+    printf("%02d/%02d/%04d" , date,month,year);
+    
+}
