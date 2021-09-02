6da946f05327ffda2fa63dbc1d27254d5bb0708f
Bertina S <76837815+bertinaa@users.noreply.github.com>
2021-09-03 01:52:36 +0530

    Create Accept Print Number.c

---
+++ Input output statements/Accept Print Number.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main(void){
+    long num1,num2;
+    scanf("%lld",&num1);
+    scanf("%lld",&num2);
+    printf("Number1 = %lld\n",num1);
+    printf("Number2 = %lld",num2);
+
+
+    return 0;
+}
