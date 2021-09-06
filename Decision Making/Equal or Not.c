c2b30f970bef9d0ab92c84b402b0b14955073085
Bertina S <76837815+bertinaa@users.noreply.github.com>
2021-09-07 00:38:51 +0530

    Create Equal or Not.c

---
+++ Decision Making/Equal or Not.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+#include <math.h>
+int main()
+{
+    int a,b;
+    scanf("%d %d",&a,&b);
+    if (a==b){
+        printf("Equal");
+    }else{
+        printf("Not Equal");
+    }
+    return 0;
+}
