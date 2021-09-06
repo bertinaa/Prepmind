a47dd5b741dfed031606f79eb8c3fa07b5bba094
Bertina S <76837815+bertinaa@users.noreply.github.com>
2021-09-07 00:38:10 +0530

    Create Print The Number With Sign.c

---
+++ Decision Making/Print The Number With Sign.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <math.h>
+int main()
+{
+    int n;
+
+    scanf("%d",&n);
+    if(n<0){
+        printf("%d",n);
+    }else if(n>0){
+        printf("+%d",n);
+    }else{
+        printf("0");
+    }
+    return 0;
+}
