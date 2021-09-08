93b5c073c98f7b1974d4296037be5923403cbb69
Bertina S <76837815+bertinaa@users.noreply.github.com>
2021-09-08 18:35:28 +0530

    Create Divisible By 5.c

---
+++ Decision Making/Divisible By 5.c
@@ -0,0 +1,13 @@
+#include<math.h>
+#include <stdio.h>
+int main ()
+{
+    int a;
+    scanf("%d",&a);
+    if(a%5==0){
+        printf("Yes");
+    }else{
+        printf("No");
+    }
+    return 0;
+}
