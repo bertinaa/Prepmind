535861b9732afb5f871dba17466429994cac0a7e
Bertina S <76837815+bertinaa@users.noreply.github.com>
2021-09-16 12:01:29 +0530

    Create Print Last Two Digit.c

---
+++ Level_1_Number_Crunching/Print Last Two Digit.c
@@ -0,0 +1,10 @@
+// Online C compiler to run C program online
+#include <stdio.h>
+
+int main() {
+    // Write C code here
+    long num;
+    scanf("%ld",&num);
+    printf("%ld",(num%100));
+    return 0;
+}
