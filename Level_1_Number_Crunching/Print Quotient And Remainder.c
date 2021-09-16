f912f6dedb36d10657e44034ae49ea79c11d8ae8
Bertina S <76837815+bertinaa@users.noreply.github.com>
2021-09-16 12:00:05 +0530

    Create Print Quotient And Remainder.c

---
+++ Level_1_Number_Crunching/Print Quotient And Remainder.c
@@ -0,0 +1,19 @@
+// Online C compiler to run C program online
+#include <stdio.h>
+
+int main() {
+    // Write C code here
+    int num;
+    scanf("%d",&num);
+    printf("%d/1 = %d\n",num,num/1);
+    printf("%d%1 = %d\n",num,num%1);
+    printf("%d/10 = %d\n",num,num/10);
+    printf("%d%10 = %d\n",num,num%10);
+    printf("%d/100 = %d\n",num,num/100);
+    printf("%d%100 = %d\n",num,num%100);
+    printf("%d/1000 = %d\n",num,num/1000);
+    printf("%d%1000 = %d\n",num,num%1000);
+    printf("%d/10000 = %d\n",num,num/10000);
+    printf("%d%10000 = %d\n",num,num%10000);
+    return 0;
+}
