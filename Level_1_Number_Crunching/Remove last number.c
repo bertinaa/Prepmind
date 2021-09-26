8f0d2d2ebdd8787974c6e783da3161a0c7c9ca17
Bertina S <76837815+bertinaa@users.noreply.github.com>
2021-09-26 19:12:35 +0530

    Create Remove last number.c

---
+++ Level_1_Number_Crunching/Remove last number.c
@@ -0,0 +1,12 @@
+// Online C compiler to run C program online
+#include <stdio.h>
+
+int main() {
+    // Write C code here
+    long n;
+    scanf("%ld",&n);
+    n=n/10;
+    printf("%ld",n);
+
+    return 0;
+}
