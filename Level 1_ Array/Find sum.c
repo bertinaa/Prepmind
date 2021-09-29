93fb364d0c600b475589877d088fbdecd47682df
Bertina S <76837815+bertinaa@users.noreply.github.com>
2021-09-29 12:00:55 +0530

    Create Find sum.c

---
+++ Level 1_ Array/Find sum.c
@@ -0,0 +1,15 @@
+// Online C compiler to run C program online
+#include <stdio.h>
+
+int main() {
+    // Write C code here
+    long long n, index,sum=0;
+    scanf("%lld", &n);
+    long long arr[n];
+    for(int i=0; i<n;i++){
+        scanf("%lld",&arr[i]);
+        sum = sum + arr[i];
+    }
+    printf("%lld",sum);
+    return 0;
+}
