3aa1036ccadc40f2a35d6f6dc4c372ec45d77084
Bertina S <76837815+bertinaa@users.noreply.github.com>
2021-09-30 09:55:56 +0530

    Create Even Numbers.c

---
+++ Level 1_ Array/Even Numbers.c
@@ -0,0 +1,17 @@
+// Online C compiler to run C program online
+#include <stdio.h>
+
+int main() {
+    // Write C code here
+    long long n, index,sum=0;
+    scanf("%lld", &n);
+    long long arr[n];
+    for(int i=0; i<n;i++){
+        scanf("%lld",&arr[i]);
+        if (arr[i]%2 ==0){
+        printf("%lld ",arr[i]);
+    }
+    
+    }
+    return 0;
+}
