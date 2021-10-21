e10f2c86a73bf68db8d8e2477ca13cefef7320e5
Bertina S <76837815+bertinaa@users.noreply.github.com>
2021-10-21 17:17:22 +0530

    Update Swap two numbers.c

--- Level 1 Bitwise/Swap two numbers.c
+++ Level 1 Bitwise/Swap two numbers.c
@@ -1,1 +1,19 @@
 
+// Online C compiler to run C program online
+#include <stdio.h>
+
+int main() {
+    // Write C code here
+    long num1,num2;
+    scanf("%ld %ld",&num1,&num2);
+    num1 = num1 + num2;
+    num2 = num1 - num2;
+    num1 = num1 - num2;
+    /*a = 10, b = 5;
+     a = 15
+     b= 10
+     a = 5
+    */
+    printf("%ld %ld",num1,num2);
+    return 0;
+}
