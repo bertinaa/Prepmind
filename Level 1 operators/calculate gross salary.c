b6fddaa78573128228a03546e6886e5b5fc910b9
Bertina S <76837815+bertinaa@users.noreply.github.com>
2021-11-25 19:03:57 +0530

    Update calculate gross salary.c

--- Level 1 operators/calculate gross salary.c
+++ Level 1 operators/calculate gross salary.c
@@ -4,7 +4,10 @@ int main()
     int n;
     scanf("%d",&n);
     double gross;
-    gross=((n/100)*60)+n;
+    /*gross=((n/100)*60)+n;
+    we can't do 40/100 because that will mean that the answer is 0
+    */
+    gross = n+ 40*n/100 + 20*n/100;
     printf("Gross Salary is %.0lf",gross);
 
 
