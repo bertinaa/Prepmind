d1f3653f4ffb94900847959e6680b4724786da47
Bertina S <76837815+bertinaa@users.noreply.github.com>
2021-11-27 12:37:25 +0530

    Update Vowel or Consonant.c

--- Level_1_Switch/Vowel or Consonant.c
+++ Level_1_Switch/Vowel or Consonant.c
@@ -1,22 +1,23 @@
 #include<stdio.h>
+#include<string.h>
+#include<ctype.h>
+
 int main()
 {
     char ch;
     scanf("%c",&ch);
-    if(ch>=65 && ch<=90 || ch>=97 && ch<=122)
+    ch = toupper(ch);
+    if(ch>=65 && ch<=90)
     {
         switch(ch)
         {
             case 'A':
-            case 'a':
             case 'E':
-            case 'e':
             case 'I':
-            case  'i':
             case 'O':
-            case 'o':
             case 'U':
-            case 'u':printf("Vowel");break;
+                printf("Vowel");
+                break;
             default : printf("Consonant");
         }
     }
