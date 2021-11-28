5de9e5d50b6a0e5a37ddb2ff3bb7bdeedb93cda7
Bertina S <76837815+bertinaa@users.noreply.github.com>
2021-11-28 14:48:54 +0530

    Update Number of Days in a month.c

--- Level_1_Switch/Number of Days in a month.c
+++ Level_1_Switch/Number of Days in a month.c
@@ -5,43 +5,24 @@ int main()
 {
     int n;
     scanf("%d",&n);
-    //3,5,7,8,10,12:
     switch(n){
         case 1:
+        case 3:
+        case 5:
+        case 7:
+        case 8:
+        case 10:
+        case 12:
             printf("31 Days");
             break;
         case 2:
             printf("28 Days");
             break;
-        case 3:
-            printf("31 Days");
-            break;
         case 4:
-            printf("30 Days");
-            break;
-        case 5:
-            printf("31 Days");
-            break;
         case 6:
-            printf("30 Days");
-            break;
-        case 7:
-            printf("31 Days");
-            break;
-        case 8:
-            printf("31 Days");
-            break;
         case 9:
-            printf("30 Days");
-            break;
-        case 10:
-            printf("31 Days");
-            break;
         case 11:
             printf("30 Days");
-            break;
-        case 12:
-            printf("31 Days");
             break;
         default:
             printf("Invalid input!");
